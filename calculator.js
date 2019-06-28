d2e630d7ed4e741a202ee3cb41acd3d0d456bf33
Brody Currie <brodycurrie@gmail.com>
2019-06-28 15:02:47 -0400

    completed reinforcement exercise

--- calculator.js
+++ calculator.js
@@ -1,10 +1,20 @@
-function add() {
+function add(num1, num2) {
+  if (num1 >= 0 && num2 >= 0) {
+    return num1 + num2;
+  };
+};
+
+function subtract(num1, num2) {
+  return num1 - num2;
 }
 
-function subtract() {
-}
-
-function sum() {
+function sum(array) {
+  if (array.length === 0) {
+    return 0;
+  } else {
+    const reducer = (total, currentVal) => total + currentVal;
+    return array.reduce(reducer);
+  }
 }
 
 module.exports = {add, subtract, sum}
